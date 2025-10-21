176fe2b56a77211952e5ffc653359417cf3c95ee
aler-hp <autademontag@gmail.com>
2025-10-21 08:20:26 -0500

    pruebas y GRAPHshow()

--- grafos.c
+++ grafos.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define vertex int
-
+/* Un Graph es un puntero para un graph. */
+typedef struct graph *Graph;
+/* La lista de adyacencia de un vértice v está formada por nodos de tipo nodo. Cada
+nodo de la lista corresponde a un arco y contiene un vecino w de v y la dirección
+del siguiente nodo de la lista. Un link es un puntero a un nodo. */
+typedef struct node *link;
 /* REPRESENTACIÓN POR LISTAS DE ADYACENCIA: La estructura graph representa un
 grafo. El campo adj es un puntero al vector de listas de adyacencia, el campo V
 contiene el número de vértices y el campo A contiene el número de arcos del grafo.
@@ -11,12 +17,7 @@ struct graph {
     int A;
     link *adj;
 };
-/* Un Graph es un puntero para un graph. */
-typedef struct graph *Graph;
-/* La lista de adyacencia de un vértice v está formada por nodos de tipo nodo. Cada
-nodo de la lista corresponde a un arco y contiene un vecino w de v y la dirección
-del siguiente nodo de la lista. Un link es un puntero a un nodo. */
-typedef struct node *link;
+
 struct node {
     vertex w;  //value
     link next;
@@ -24,7 +25,7 @@ struct node {
 /* La función NEWnode() recibe un vértice w y la dirección next de un nodo y
 devuelve la dirección a de un nuevo nodo tal que a->w == w y a->next == next. */
 static link NEWnode( vertex w, link next) {
-    link a = malloc( sizeof (struct node));
+    link a = (link) malloc( sizeof (struct node));
     a->w = w;
     a->next = next;
     return a;
@@ -33,10 +34,10 @@ static link NEWnode( vertex w, link next) {
 /* REPRESENTACIÓN POR LISTAS DE ADYACENCIA: La función GRAPHinit() construye un grafo con
 vértices 0 1 .. V-1 y ningún arco. */
 Graph GRAPHinit( int V) {
-    Graph G = malloc( sizeof *G);
+    Graph G = (Graph) malloc( sizeof *G);
     G->V = V;
     G->A = 0;
-    G->adj = malloc( V * sizeof (link));
+    G->adj = (link *) malloc( V * sizeof (link));
     for (vertex v = 0; v < V; ++v)
         G->adj[v] = NULL;
     return G;
@@ -69,16 +70,27 @@ int GRAPHindeg(Graph G, vertex v) {
   return count;
 }
 
+void GRAPHshow(Graph G) {
+    for (vertex v = 0; v < G->V; v++) {
+        printf("Vertice %d :", v);
+        for (link a = G->adj[v]; a != NULL; a = a->next)
+            printf("%d--", a->w);
+        printf("\n");
+    }
+}
+
 
 int main(void) {
     Graph G = GRAPHinit(5); 
 
     GRAPHinsertArc(G, 0, 1);
-    GRAPHinsertArc(G, 0, 2);
-    GRAPHinsertArc(G, 1, 2);
+    GRAPHinsertArc(G, 0, 3);
+    GRAPHinsertArc(G, 1, 4);
     GRAPHinsertArc(G, 2, 3);
     GRAPHinsertArc(G, 3, 4);
     GRAPHinsertArc(G, 4, 0);
+
+    GRAPHshow(G);
 
     return 0;
 }
